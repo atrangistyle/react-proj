2051cb1397a8b1f2d4335925782ae5f73a662ba2
jyoti pathak <jyoti.pathak@juspay.in>
2023-07-27 23:06:16 +0530

    init

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+/*import logo from './logo.svg';*/
+import './App.css';
+import { Greet } from './components/greet'
+
+function App() {
+  return (
+    <div className="App">
+      <Greet></Greet>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/greet.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// function Greet() {
+
+//     return <h1> Hello J </h1>
+// }
+export  const Greet = () => <h1>hello Jyo </h1>
+// export default Greet
